d620517ddba8d93927d3a1816fc08b3accf6aa34
TaniaTenorio <taniatenorio@hotmail.com>
2020-12-04 22:58:41 -0600

    create and use card component

--- src/pages/Secret/Secret.page.jsx
+++ src/pages/Secret/Secret.page.jsx
@@ -1,24 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Secret.styles.css';
+import { useHistory } from 'react-router';
+import Card from '../../components/Card';
+
+import { videoData } from '../../utils/dataMock';
 
 function SecretPage() {
+  const [loading, setLoading] = useState(true);
+  const [value, setValue] = useState('');
+  const [searchData, setSearchData] = useState([]);
+
+  const history = useHistory();
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch(
+  //     `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=popular&type=video&key=AIzaSyAJo6nlfpXltr75BUwKhCabevhoDK4CndM`
+  //   )
+  //     .then((res) => res.json())
+  //     .then(({ items }) => {
+  //       setLoading(false);
+  //       setSearchData(items);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    setSearchData(videoData);
+    setLoading(false);
+  }, []);
+
+  const fetchData = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=${value}&type=video&key=AIzaSyAJo6nlfpXltr75BUwKhCabevhoDK4CndM`
+    )
+      .then((res) => res.json())
+      .then(({ items }) => {
+        setSearchData(items);
+      });
+  };
+
   return (
-    <section>
-      <pre>
-        welcome, voyager...
-        <Link to="/"> ← go back</Link>
-      </pre>
-      <iframe
-        width="800"
-        height="450"
-        allowFullScreen
-        frameBorder="0"
-        title="rick roll"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      />
-    </section>
+    <main className="secret-view">
+      <header className="secret-header">
+        <p>YOUR VIDEOS</p>
+        <form onSubmit={fetchData}>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button type="submit">Go!</button>
+        </form>
+      </header>
+      <section className="main-container">
+        <section className="cards-container">
+          {!loading &&
+            searchData.items.map((item) => {
+              return (
+                <Card
+                  key={item.etag}
+                  source={item.snippet.thumbnails.default.url}
+                  title={item.snippet.title}
+                  description={item.snippet.description}
+                  onClickHandler={() => history.push(`/video/${item.id.videoId}`)}
+                />
+              );
+            })}
+        </section>
+      </section>
+      <footer className="secret-footer">
+        <pre>
+          welcome, voyager...
+          <Link to="/"> ← go back</Link>
+        </pre>
+      </footer>
+    </main>
   );
 }
 
 export default SecretPage;
+
+// https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=songs&type=video&key=AIzaSyAJo6nlfpXltr75BUwKhCabevhoDK4CndM
---
+++ src/utils/dataMock.js
@@ -0,0 +1,371 @@
+export const videoData = {
+  kind: 'youtube#searchListResponse',
+  etag: 'xc3SPxIi59VtEMGZ7Qt4Y_njbek',
+  nextPageToken: 'CAoQAA',
+  regionCode: 'MX',
+  pageInfo: {
+    totalResults: 1000000,
+    resultsPerPage: 10,
+  },
+  items: [
+    {
+      kind: 'youtube#searchResult',
+      etag: 'neo3WMPD9OF1ucVdykJZtmNvFxY',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'qlrhF4Xz6ho',
+      },
+      snippet: {
+        publishedAt: '2020-11-30T10:06:01Z',
+        channelId: 'UC6K7yzj2kV3v3sASdaYCY0Q',
+        title:
+          'Hindi Romantic Songs 2020 November - Latest Indian Songs 2020 November - Hindi New Songs 2020',
+        description:
+          'Hindi Romantic Songs 2020 November - Latest Indian Songs 2020 November - Hindi New Songs 2020 ...',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/qlrhF4Xz6ho/default_live.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/qlrhF4Xz6ho/mqdefault_live.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/qlrhF4Xz6ho/hqdefault_live.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Bollywood Hits Songs',
+        liveBroadcastContent: 'live',
+        publishTime: '2020-11-30T10:06:01Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: '5nU7vGL4cMXPWJnD5fjovOJtuJ8',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'I0zDuWb5K3k',
+      },
+      snippet: {
+        publishedAt: '2020-11-30T00:03:10Z',
+        channelId: 'UCJ11MX8RZVA9FkU8o-eh6rQ',
+        title:
+          'Top Bollywood Songs November 2020 Live - Hindi Heart Touching Songs 2020 - Indian New Songs Live',
+        description:
+          "Top Bollywood Songs November 2020 Live - Hindi Heart Touching Songs 2020 - Indian New Songs Live ------------------------------------------- Don't Forget Like and ...",
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/I0zDuWb5K3k/default_live.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/I0zDuWb5K3k/mqdefault_live.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/I0zDuWb5K3k/hqdefault_live.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Romantic Music Box',
+        liveBroadcastContent: 'live',
+        publishTime: '2020-11-30T00:03:10Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: 'j7D6FIHjubcVEt9o_va7V2D0HG4',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'dcH-b3R96C8',
+      },
+      snippet: {
+        publishedAt: '2020-11-30T09:47:59Z',
+        channelId: 'UCudwNYFE5jCpfRdoz6iGwXA',
+        title:
+          'New Songs 2020 ☄️ Top English Songs Collection 2020 ☄️ Best Pop Music Playlist 2020',
+        description:
+          'New Songs 2020 ☄️ Top English Songs Collection 2020 ☄️ Best Pop Music Playlist 2020 ➡ Time Music Fanpage: https://bitly.com.vn/DkK9Y ➡ Time Music ...',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/dcH-b3R96C8/default_live.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/dcH-b3R96C8/mqdefault_live.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/dcH-b3R96C8/hqdefault_live.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Time Music',
+        liveBroadcastContent: 'live',
+        publishTime: '2020-11-30T09:47:59Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: 'MEi4BJ5a2iONSoZPebSOAqcgY9k',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'GMIWUYT5Xo0',
+      },
+      snippet: {
+        publishedAt: '2020-11-30T14:53:20Z',
+        channelId: 'UClmVNdmUIRLVIQJ3jBfQq_g',
+        title:
+          'Mariah Carey, Ariana Grande, Justin Bieber Christmas Songs 🎅🎄 Top Pop Christmas Songs Playlist 2021',
+        description:
+          'Mariah Carey, Ariana Grande, Justin Bieber Christmas Songs Top Pop Christmas Songs Playlist 2021 #christmasfireplace #christmassong #christmasmusic ...',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/GMIWUYT5Xo0/default_live.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/GMIWUYT5Xo0/mqdefault_live.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/GMIWUYT5Xo0/hqdefault_live.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Peaceful music - MTV Music',
+        liveBroadcastContent: 'live',
+        publishTime: '2020-11-30T14:53:20Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: 'OZeKMSbAPMKNH-NjIvj4T8szlgQ',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'LAQQ25sIrlo',
+      },
+      snippet: {
+        publishedAt: '2020-11-30T10:17:37Z',
+        channelId: 'UC1-vMrm8aCgKfHi_75XpFvw',
+        title:
+          'Best Classic Acoustic Songs Of All Time - Acoustic Cover Of Popular Songs - Guitar Acoustic Music',
+        description:
+          'Best Classic Acoustic Songs Of All Time - Acoustic Cover Of Popular Songs - Guitar Acoustic Music Best Classic Acoustic Songs Of All Time - Acoustic Cover Of ...',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/LAQQ25sIrlo/default_live.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/LAQQ25sIrlo/mqdefault_live.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/LAQQ25sIrlo/hqdefault_live.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Acoustic Greatest Hits.',
+        liveBroadcastContent: 'live',
+        publishTime: '2020-11-30T10:17:37Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: 'xgQe03i03qMgYUZVZYQmCoRsz0o',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'BEQ65zDozlo',
+      },
+      snippet: {
+        publishedAt: '2020-11-30T00:00:45Z',
+        channelId: 'UCsPNKAwzONcP_n8rVBYfyCg',
+        title:
+          'Old Vs New Bollywood Mashup Songs | New Romantic Mashup Nov 2020 |Old Hindi Songs Remix MaShup',
+        description:
+          "Old Vs New Bollywood Mashup Songs | New Romantic Mashup Nov 2020 |Old Hindi Songs Remix MaShup -------------------------------------------------- Don't forget to ...",
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/BEQ65zDozlo/default_live.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/BEQ65zDozlo/mqdefault_live.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/BEQ65zDozlo/hqdefault_live.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Atif Aslam 2020',
+        liveBroadcastContent: 'live',
+        publishTime: '2020-11-30T00:00:45Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: 'PHuY2b2fnMNMuA0MZQXV_rh15d0',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'L6rK3e7mwcI',
+      },
+      snippet: {
+        publishedAt: '2020-11-29T10:56:48Z',
+        channelId: 'UCnZx--LpG2spgmlxOcC-DRA',
+        title: 'When you accidentally write songs that already exist part 2',
+        description:
+          'Get Honey for FREE today ▸ http://joinhoney.com/thrasher Honey finds coupons with the click of a button. Thanks to Honey for sponsoring!',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/L6rK3e7mwcI/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/L6rK3e7mwcI/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/L6rK3e7mwcI/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Daniel Thrasher',
+        liveBroadcastContent: 'none',
+        publishTime: '2020-11-29T10:56:48Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: 'eNkUffmqYuS04aP2xvxwR5jrVB0',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'thGd7rrraiI',
+      },
+      snippet: {
+        publishedAt: '2020-11-29T20:39:40Z',
+        channelId: 'UCoQg8OyMj8-7UkXJRZ4fLSQ',
+        title:
+          'Old vs New Bollywood Mashup Songs 2020| Romantic Hindi Love MASHUP Remix songs_90&#39;s Bollywood_Mashup',
+        description:
+          "Old vs New Bollywood Mashup Songs 2020| Romantic Hindi Love MASHUP Remix songs_90's Bollywood_Mashup https://youtu.be/thGd7rrraiI ...",
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/thGd7rrraiI/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/thGd7rrraiI/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/thGd7rrraiI/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Bollywood Mashup.',
+        liveBroadcastContent: 'none',
+        publishTime: '2020-11-29T20:39:40Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: 'lfD7jSvKIpmnUTdHQJ9V4ThFvXM',
+      id: {
+        kind: 'youtube#video',
+        videoId: 'RJBvlcP1zbQ',
+      },
+      snippet: {
+        publishedAt: '2020-11-30T18:50:43Z',
+        channelId: 'UCoQg8OyMj8-7UkXJRZ4fLSQ',
+        title:
+          'Old vs New Bollywood Mashup Songs 2020 | 90s Hindi Remix Mashup : Old Is Gold_InDiAN MaShUp 2020',
+        description:
+          'Old vs New Bollywood Mashup Songs 2020 | 90s Hindi Remix Mashup \\ Old Is Gold mashup_InDiAn MaSHup 2020 https://youtu.be/RJBvlcP1zbQ.',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/RJBvlcP1zbQ/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/RJBvlcP1zbQ/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/RJBvlcP1zbQ/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Bollywood Mashup.',
+        liveBroadcastContent: 'none',
+        publishTime: '2020-11-30T18:50:43Z',
+      },
+    },
+    {
+      kind: 'youtube#searchResult',
+      etag: 'edYmKkaPe-jzaQTLpLfqCuiNIsU',
+      id: {
+        kind: 'youtube#video',
+        videoId: '4VYM4Jq5WkU',
+      },
+      snippet: {
+        publishedAt: '2020-11-29T13:15:00Z',
+        channelId: 'UCRp--eWwsLI_uIkCnsbfwFQ',
+        title:
+          'Best Christmas Songs Playlist 🎅🏼 Christmas Music Mix 🎄 Top Christmas Songs 2020',
+        description:
+          'Best Christmas Songs Playlist Christmas Music Mix Top Christmas Songs 2020 The best Christmas Songs mix ever. Our 1 hour Christmas Music mix ...',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/4VYM4Jq5WkU/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/4VYM4Jq5WkU/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/4VYM4Jq5WkU/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'Holiday Party Music',
+        liveBroadcastContent: 'none',
+        publishTime: '2020-11-29T13:15:00Z',
+      },
+    },
+  ],
+};
